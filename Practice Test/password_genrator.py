2042a17f0edd19b8fcd381b09188252427346496
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-06-07 10:02:36 +0800

    added try again

--- Practice Test/password_genrator.py
+++ Practice Test/password_genrator.py
@@ -1,5 +1,6 @@
 from secrets import choice
 from string import ascii_letters, digits, punctuation
+import os, sys
 
 def pass_gen(length: int) -> str:
     return ''.join(choice(ascii_letters + digits + punctuation) for _ in range(length))
@@ -12,9 +13,22 @@ def main() -> None:
         if num <= 0:
             print("Please enter a positive integer.")
         else:
-            print(pass_gen(num))
+            print(f"Password: {pass_gen(num)}")
+        again()
     except ValueError:
         print("Invalid input. Please enter an integer.")
+
+def again(): #Run again
+    print("==================================")
+    ask = input("Do you want to try again (y/n): ")
+    print("==================================")
+    if ask == "y" or ask == "Y":
+        main()
+    elif ask == "n" or ask == "N":
+        sys.exit()
+    else:
+        print("Invalid Input! Try again.")
+        again()
 
 if __name__ == '__main__':
     main()
